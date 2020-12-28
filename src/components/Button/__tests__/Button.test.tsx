fed633df458664c4cb51117e65d468d650755750
Rudinei Silva <rudnei@fuerzastudio.com.br>
2020-12-28 07:05:56 -0300

    tests: updating snapshots & adding test cases for button standart component

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,9 +1,31 @@
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 import * as React from "react";
 
 import Button from "..";
 
-test("<Button/> snapshot", () => {
-  const tree = render(<Button />);
-  expect(tree).toMatchSnapshot();
+describe("Standart Button component test cases", () => {
+  test("<Button/> snapshot", () => {
+    const tree = render(<Button />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("Should Display children inside button text", () => {
+    const props = {
+      children: "button text",
+    };
+
+    const { getByText } = render(<Button {...props} />);
+    getByText(props.children);
+  });
+
+  test("Should execute onPress function as expected", () => {
+    const props = {
+      children: "Press me",
+      onPress: jest.fn(),
+    };
+
+    const { getByText } = render(<Button {...props} />);
+    fireEvent.press(getByText("Press me"));
+    expect(props.onPress).toHaveBeenCalled();
+  });
 });
